168aceaf4b3f5d6c7f11e17001100af494a09fc9
Bhumireddygayathri <gayathribhumireddy777@gmail.com>
2019-05-30 04:50:12 -0700

    employee model

---
+++ Demogt/src/Employee.java
@@ -0,0 +1,28 @@
+public class Employee {
+
+	private int Empid;
+	private String Empname;
+	private double sal;
+	public Employee() {
+	}
+	public int getEmpid() {
+		return Empid;
+	}
+	public void setEmpid(int empid) {
+		Empid = empid;
+	}
+	public String getEmpname() {
+		return Empname;
+	}
+	public void setEmpname(String empname) {
+		Empname = empname;
+	}
+	public double getSal() {
+		return sal;
+	}
+	public void setSal(double sal) {
+		this.sal = sal;
+	}
+	
+}
+
---
+++ Demogt/src/Testemploye.java
@@ -0,0 +1,12 @@
+public class Testemploye {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Employee e=new Employee();
+e.setEmpid(101);
+e.setEmpname("gayathri");
+e.setSal(40000);
+
+	}
+
+}
